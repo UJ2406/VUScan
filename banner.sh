319929e6b8956f6c358489d4a834e9e7ee324c49
UJ2406 <143389843+UJ2406@users.noreply.github.com>
2023-12-06 10:30:27 +0530

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "************************************"
+echo "*                                  *"
+echo "*       WELCOME TO VUSCAN          *"
+echo "*        For open ports            *"
+echo "*                                  *"
+echo "*                                  *"
+echo "*           MADE BY-               *"
+echo "*         Ujjwal Singh             *"
+echo "*               &                  *"
+echo "*          Ronit Vats              *"
+echo "*                                  *"
+echo "************************************"
+
+
+exit 0
